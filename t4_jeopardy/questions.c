e2b8401ea114897e0dda0684bfb50d4421139cce
joshualemmon <joshua.lemmon@uoit.net>
2017-02-07 11:59:01 -0500

    Finished writing questions, need to put into struct form

--- t4_jeopardy/questions.c
+++ t4_jeopardy/questions.c
@@ -14,13 +14,18 @@
 void initialize_game(void)
 {
 	// initialize each question struct and assign it to the questions array
-	questions[0] = {"programming","This statement allows you to check a boolean condition.", "What is an if statement?",100,false};
-	questions[1] = {"algorithms", "This sorting algorithm is O(n^2).", "What is bubble sort?",100,false};
-	questions[2] = {"databases", "This is a prearranged collection of data.", "What is a database.", 100, false};
-	questions[3] = {"programming", "This programming paradigm involves lambda calculus.", "What is functional programming?", 200, false};
-	questions[4] = {"algorithms", "This is the average case performance of quicksort.", "What is O(nlogn)?", 200, false};
-	questions[5] = {"databases", "This person proposed the relational model.", "Who is Edgar F. Codd?", 200, false};
-	questions[6] = 
+	questions[0] = struct question("programming","This statement allows you to check a boolean condition.", "an if statement?",100,false);
+	questions[1] = {"algorithms", "This sorting algorithm is O(n^2).", "bubble sort?",100,false};
+	questions[2] = {"databases", "This is a prearranged collection of data.", "a database.", 100, false};
+	questions[3] = {"programming", "This programming paradigm involves lambda calculus.", "functional programming?", 200, false};
+	questions[4] = {"algorithms", "This is the average case performance of quicksort.", "O(nlogn)?", 200, false};
+	questions[5] = {"databases", "This person proposed the relational model.", "Edgar F. Codd?", 200, false};
+	questions[6] = {"programming", "This was the first programming language.", "FORTRAN?", 300, false};
+	questions[7] = {"algorithms", "This form of algorithm makes the locally optimal choice at each step in hopes of a maximized solution.", "a greedy algorithm?", 300, false};
+	questions[8] = {"databases", "This is a query run by a query.", "sub-query?",300,false};
+	questions[9] = {"programming", "This procedure involves a function calling itself until a base case is reached.", "recursion?", 400, false};
+	questions[10] = {"algorithms", "This word is used to describe problems that cannot be realistically solved.", "intractable?", 400, false};
+	questions[11] = {"databases", "This word describes a set of entries with similar attriutes.", "entity type?", 400, false};
 
 
 }

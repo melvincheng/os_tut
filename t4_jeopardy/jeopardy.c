5e047205680dde5a3d9fda81973c9843927d2e1e
melvincheng <melvin.cheng@live.com>
2017-02-07 10:55:00 -0500

    Adding t4 files

---
+++ t4_jeopardy/jeopardy.c
@@ -0,0 +1,81 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 4
+
+// Put global environment variables here
+
+int main(int argc, char *argv[])
+{
+	// An array of 4 players, may need to be a pointer if you want it set dynamically
+	player players[NUM_PLAYERS];
+	
+	// Input buffer and and commands
+	char buffer[BUFFER_LEN] = { 0 };
+
+	// Display the game introduction and initialize the questions
+	initialize_game();
+
+	// Prompt for players names
+	printf("Players, please enter your names\n");
+	// initialize each of the players in the array
+	for(int i = 0; i < 4; i++)
+	{
+		fgets(buffer, BUFFER_LEN, stdin);
+		strcpy(players[i].name, buffer);
+		players[i].score = 0;
+	}
+
+	// Perform an infinite loop getting command input from users until game ends
+	while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+	{
+	// 	// Call functions from the questions and players source files
+
+	// 	// Execute the game until all questions are answered
+
+	// 	// Display the final results and exit
+		show_results(players, NUM_PLAYERS);
+	}
+	return EXIT_SUCCESS;
+}
+
+void tokenize(char *input, char **tokens)
+{
+
+}
+
+void show_results(player *players, int num_players)
+{
+	player temp_player;
+	for(int i = 0; i < NUM_PLAYERS - 1; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if(players[i].score > players[i+1].score)
+			{
+				temp_player = players[i];
+				players[i] = players[i + 1];
+				players[i + 1] = temp_player;
+			}
+		}
+	}
+
+	for(int i = 0; i < NUM_PLAYERS; i++)
+	{
+		printf("%s %d\n", players[i].name, players[i].score);
+	}
+}
---
+++ t4_jeopardy/questions.c
@@ -0,0 +1,79 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "questions.h"
+
+// Initializes the array of questions for the game
+void initialize_game(void)
+{
+	// initialize each question struct and assign it to the questions array
+
+}
+
+// Displays each of the remaining categories and question dollar values that have not been answered
+void display_categories(void)
+{
+	// print categories and dollar values for each unanswered question in questions array
+	
+}
+
+// Displays the question for the category and dollar value
+void display_question(char *category, int value)
+{
+	for(int i = 0; i < NUM_QUESTIONS; i++)
+	{
+		if(strcmp(questions[i].category, category) == 0)
+		{
+			if(questions[i].value == value)
+			{
+				printf("%s\n", questions[i].question);
+			}
+		}
+	}
+}
+
+// Returns true if the answer is correct for the question for that category and dollar value
+bool valid_answer(char *category, int value, char *answer)
+{
+	// Look into string comparison functions
+	for(int i = 0; i < NUM_QUESTIONS; i++)
+	{
+		if(strcmp(questions[i].category, category) == 0)
+		{
+			if(questions[i].value == value)
+			{
+				if(strcmp(questions[i].answer, answer) == 0)
+				{
+					return true;
+				}else{
+					return false;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+// Returns true if the question has already been answered
+bool already_answered(char *category, int value)
+{
+	// lookup the question and see if it's already been marked as answered
+	for(int i = 0; i < NUM_QUESTIONS; i++)
+	{
+		if(strcmp(questions[i].category, category) == 0)
+		{
+			if(questions[i].value == value)
+			{
+				return questions[i].answered;
+			}
+		}
+	}
+	return false;
+}

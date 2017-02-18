f19e3bbd178cd584d2fafb005f184aef60aee161
Melvin <melvin.cheng@live.com>
2017-02-18 17:07:09 -0500

    Jeopardy completed (probably)

--- t4_jeopardy/players.c
+++ t4_jeopardy/players.c
@@ -16,7 +16,7 @@ bool player_exists(player *players, int num_players, char *name)
 {
 	for(int i = 0; i < num_players; i++)
 	{
-		if(strcasecmp(players[i].name, name) == -10)
+		if(strcasecmp(players[i].name, name) == 0)
 		{
 			return true;
 		}
@@ -31,7 +31,7 @@ void update_score(player *players, int num_players, char *name, int score)
 {
 	for(int i = 0; i < num_players; i++)
 	{
-		if(strcasecmp(players[i].name, name) == -10)
+		if(strcasecmp(players[i].name, name) == 0)
 		{
 			players[i].score += score;
 			break;

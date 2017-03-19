41a0def1cab5fd18d517b2d604b96473db86447e
Melvin Cheng <melvin-cheng@live.com>
2017-03-18 22:32:08 -0400

    Adding t8q2

--- t8/t8q2.c
+++ t8/t8q2.c
@@ -7,6 +7,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MEMORY 1024
+
+typedef struct{
+	char name[256];
+	int priority;
+	int pid;
+	int address;
+	int memory;
+	int runtime;
+	bool suspended;
+} proc;
+
 int main(void){
-	
+	int avail_mem length[MEMORY] = { 0 };
 }
